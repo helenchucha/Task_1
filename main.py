6e49b4345085f4c8f56faf928ea48374120418bc
Helen Chucha <helenchucha@gmail.com>
2025-06-15 07:51:11 +0300

    Finished task

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
 # Pythagoras' pants
-# Module name:
+# Module name: Task_1
+
+from array import *
 
 def main():
-    print_hi('PyCharm')
+    print_hi()
+    pythagorean_pants()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def pythagorean_pants():
+    # Create an array of unsorted numbers.
+    arr_int = []
+    filling_an_array(arr_int)
+    a = arr_int[0] ** 2
+    b = arr_int[1] ** 2
+    c = arr_int[2] ** 2
+    if (a + b == c) or (a + c == b) or (b + c == a):
+        print(str(arr_int) + ' -> True')
+    else:
+        print(str(arr_int) + ' -> False')
+
+def filling_an_array(arr_int):
+    # Filling the array with verified data
+    for i in range(3):
+        user_input = input('Введи ' + str(i+1) + ' елемент списка: ')
+        ver_number = get_number(i, user_input, arr_int)
+        arr_int.append(ver_number)
+
+def get_number(i, user_input, arr_int):
+    # Сhecking entered numbers
+    while True:
+        try:
+            number = int(user_input)
+            return number
+        except ValueError:
+            print("Невірний ввід. Будь ласка, введіть число.")
+            user_input = input('Введи ' + str(i + 1) + ' елемент списка: ')
 
 
-# Press the green button in the gutter to run the script.
+def print_hi():
+    print(f'Привіт!')
+
 if __name__ == '__main__':
     main()
-#    print_hi('PyCharm')
